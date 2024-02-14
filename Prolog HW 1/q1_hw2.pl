ef80da95ffccd34bca46f26270f3c73bce7f045b
gavin-barro <g.ab28320@gmail.com>
2024-02-14 10:29:47 -0500

    first two questions of HW 1

---
+++ Prolog HW 1/q1_hw2.pl
@@ -0,0 +1,12 @@
+% Question 1
+
+% Facts:
+sky_is_blue.
+sunny_day :- sky_is_blue.
+people_feel_good :- sunny_day.
+
+% Query:
+people_feel_good.
+
+% OUTPUT: true
+
---
+++ Prolog HW 1/q2_hw1.pl
@@ -0,0 +1,11 @@
+% Question 2
+
+% Facts:
+have_sugar.
+have_flower.
+bake_cake :- have_flower, have_sugar.
+
+% Query
+bake_cake.
+
+% OUTPUT: true
