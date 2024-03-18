6112530283189cc63ba53a93aacaef347bda5325
gavin-barro <g.ab28320@gmail.com>
2024-03-18 13:04:38 -0400

    homework2

---
+++ HW 2/Trial.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class KnowledgeRepresentation {
+
+    public static void main(String[] args) {
+
+        // Adding goal state
+
+
+        Map<Tuple<Integer, Integer>, Object> grid = new HashMap<>();
+        
+
+
+        Puzzle puzzle = new Puzzle(grid);
+    }
+}
